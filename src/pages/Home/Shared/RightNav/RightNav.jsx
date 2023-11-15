fe107693eaf46a97701fdb57b7b89889fbacbbb2
asib ahmed <ahmedasib1@gmail.com>
2023-11-15 22:47:02 +0600

    get data from server side

--- src/pages/Home/Shared/RightNav/RightNav.jsx
+++ src/pages/Home/Shared/RightNav/RightNav.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from "react";
 
 
 const RightNav = () => {
+    const [catagories, uesCatagories] = useState();
+    useEffect(()=>{
+        fetch('http://localhost:5000/catagories')
+        .then(response => response.json())
+        .then(data =>console.log(data))
+        .catch(error => console.log(error))
+    },[])
     return (
         <div>
             <h2>right nav</h2>
