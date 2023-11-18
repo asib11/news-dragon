3df23c627fdbb3a26bd744685e8b0fe61ece70e1
asib ahmed <ahmedasib1@gmail.com>
2023-11-18 23:35:47 +0600

    news layout make

---
+++ src/layouts/NewsLayout.jsx
@@ -0,0 +1,33 @@
+import Footer from "../pages/Home/Shared/Footer/Footer";
+import Header from "../pages/Home/Shared/Header/Header";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import LeftNav from "../pages/Home/Shared/RightNav/RightNav";
+import { Outlet } from "react-router-dom";
+
+const NewsLayout = () => {
+    return (
+        <div>
+            <Header>
+
+            </Header>
+            <Container>
+                <Row>
+                    
+                    <Col lg={9}>
+                        <Outlet></Outlet>
+                    </Col>
+                    <Col lg={3}>
+                        <LeftNav></LeftNav>
+                    </Col>
+                </Row>
+            </Container>
+            <Footer>
+
+            </Footer>
+        </div>
+    );
+};
+
+export default NewsLayout;
---
+++ src/pages/news/News/News.jsx
@@ -0,0 +1,11 @@
+
+
+const News = () => {
+    return (
+        <div>
+            <h2>comming soon...</h2>
+        </div>
+    );
+};
+
+export default News;
