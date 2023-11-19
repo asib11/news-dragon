49794718f05ce306a838d5cb9101c6297421fb9d
asib ahmed <ahmedasib1@gmail.com>
2023-11-19 17:52:53 +0600

    catagory work

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,11 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
+import NewsCard from "../NewsCard/NewsCard";
 
 
 const Category = () => {
-    let {id} = useParams()
+    let {id} = useParams();
+    const categoryNews = useLoaderData()
     return (
         <div>
-            <h2>this is catagory {id}</h2>
+            <h2>this is catagory: {categoryNews.length}</h2>
+            {
+                categoryNews.map(news => <NewsCard
+                key = {news._id}
+                news = {news}
+                ></NewsCard> )
+            }
         </div>
     );
 };
